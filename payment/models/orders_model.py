cba8729d9a31e24280199b1679eb7d6616182ab4
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2023-11-10 02:05:09 +0400

    Add models

---
+++ payment/models/orders_model.py
@@ -0,0 +1,14 @@
+from redis_om import HashModel
+
+
+# Order Model for Database
+class Order(HashModel):
+    product_id: str
+    price: float
+    fee: float
+    total: float
+    quantity: int
+    status: str  # pending, completed, refunded
+
+    class Meta:
+        database = None
